31cd8c80272e8051def3b827e24fa68c384d40bb
Nguyen Thuy Duong <zaika.nope11@gmail.com>
2026-05-06 14:41:17 +0700

    [STYLE] Create about section layout

--- exercises/02_about_skills/script.js
+++ exercises/02_about_skills/script.js
@@ -2,8 +2,9 @@ const skills = document.querySelectorAll('.skill-progress');
 
 window.addEventListener('scroll', () => {
     skills.forEach(skill => {
-        const position = skill.getBoundingClientRect().top;
-        if (position < window.innerHeight) {
+        const pos = skill.getBoundingClientRect().top;
+
+        if (pos < window.innerHeight) {
             skill.style.width = skill.getAttribute('data-width');
         }
     });
